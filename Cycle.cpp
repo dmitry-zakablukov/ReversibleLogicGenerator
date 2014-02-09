389d3e84dd02a586f61b179627daae5dac4c7905
Dmitry Zakablukov <dmitriy.zakablukov@gmail.com>
2014-02-09 13:17:39 +0400

    [4.0] Cycle cleaned-up

--- Cycle.cpp
+++ Cycle.cpp
@@ -6,14 +6,12 @@ namespace ReversibleLogic
 Cycle::Cycle()
     : elements()
     , finalized(false)
-    , previousMultiplicationType(mtNone)
 {
 }
 
 Cycle::Cycle(vector<word>&& source)
     : elements( move(source) )
     , finalized(false)
-    , previousMultiplicationType(mtNone)
 {
     uint elementCount = length();
     if(elements[0] == elements[elementCount - 1])
@@ -169,18 +167,6 @@ word Cycle::getOutput(word input, shared_ptr<list<Transposition>> transpositions
     return output;
 }
 
-word Cycle::getOutput(word input, const list<shared_ptr<Cycle>>& cycles) const
-{
-    word output = input;
-    forcin(iter, cycles)
-    {
-        const Cycle& cycle = **iter;
-        output = cycle.getOutput(output);
-    }
-
-    return output;
-}
-
 uint Cycle::modIndex(uint index) const
 {
     uint elementCount = length();
@@ -195,43 +181,6 @@ uint Cycle::modIndex(uint index, uint mod) const
 
     resIndex -= (resIndex >= mod) * mod;
     return resIndex;
-}
-
-list<shared_ptr<Cycle>> Cycle::multiplyByTranspositions(
-    const unordered_set<word>& targetElements, bool isLeftMultiplication,
-    bool* hasNewCycles)
-{
-    *hasNewCycles = false;
-    list<shared_ptr<Cycle>> restCycles;
-
-    DisjointResult* disjointResult = (isLeftMultiplication ? &leftDisjointResult : &rightDisjointResult);
-
-    bool multipliedFlag = false;
-    shared_ptr<list<Transposition>> transpositions = disjointResult->transpositions;
-
-    for(auto iter = transpositions->begin(); iter != transpositions->end(); )
-    {
-        Transposition& transp = *iter;
-        word x = transp.getX();
-
-        if(targetElements.count(x))
-        {
-            iter = transpositions->erase(iter);
-            multipliedFlag = true;
-        }
-        else
-        {
-            ++iter;
-        }
-    }
-
-    if(multipliedFlag)
-    {
-        *hasNewCycles = true;
-        restCycles = getRestCycles(isLeftMultiplication);
-    }
-
-    return restCycles;
 }
 
 void Cycle::multiplyByTranspositions(shared_ptr<list<Transposition>> transpositions,
@@ -287,83 +236,6 @@ void Cycle::multiplyByTranspositions(shared_ptr<list<Transposition>> transpositi
 
     assert(!nextCycle->length(),
         string("Cycle::multiplyByTranspositions() failed because of last cycle"));
-}
-
-list<shared_ptr<Cycle>> Cycle::getRestCycles(bool isLeftMultiplication)
-{
-    DisjointResult* disjointResult = (isLeftMultiplication ? &leftDisjointResult : &rightDisjointResult);
-    shared_ptr<list<Transposition>> transpositions = disjointResult->transpositions;
-    const list<shared_ptr<Cycle>> restCycles = disjointResult->restCycles;
-
-    list<shared_ptr<Cycle>> newCycles;
-    getCyclesAfterMultiplication(isLeftMultiplication, transpositions, restCycles, &newCycles);
-
-    return newCycles;
-}
-
-void Cycle::getCyclesAfterMultiplication(bool isLeftMultiplication,
-    shared_ptr<list<Transposition>> transpositions,
-    const list<shared_ptr<Cycle>>& cycles, list<shared_ptr<Cycle>>* result)
-{
-    if(!transpositions->size())
-    {
-        *result = cycles;
-    }
-    else
-    {
-        // multiply transpositions on rest cycles
-
-        unordered_set<word> visitedElements;
-        shared_ptr<Cycle> nextCycle(new Cycle());
-
-        uint elementCount = length();
-        for(uint index = 0; index < elementCount; ++index)
-        {
-            word x = elements[index];
-            if(!visitedElements.count(x))
-            {
-                while(!nextCycle->isFinal())
-                {
-                    word y = x;
-                    if(isLeftMultiplication)
-                    {
-                        y = getOutput(y, transpositions);
-                        y = getOutput(y, cycles);
-                    }
-                    else
-                    {
-                        y = getOutput(y, cycles);
-                        y = getOutput(y, transpositions);
-                    }
-
-                    if(nextCycle->isEmpty())
-                    {
-                        nextCycle->append(x);
-                    }
-                    nextCycle->append(y);
-
-                    visitedElements.insert(x);
-                    x = y;
-                }
-
-                uint cycleLength = nextCycle->length();
-
-                // skip fixed point
-                if(cycleLength > 1)
-                {
-                    result->push_back(nextCycle);
-                }
-
-                nextCycle = shared_ptr<Cycle>(new Cycle());
-            }
-        }
-
-        assert(visitedElements.size() == elementCount,
-            string("Cycle::getCyclesAfterMultiplication() failed because not all elements processed"));
-
-        assert(!nextCycle->length(),
-            string("Cycle::getCyclesAfterMultiplication() failed because of last cycle"));
-    }
 }
 
 void Cycle::prepareForDisjoint(unordered_map<word, uint>* frequencyMap)
@@ -462,89 +334,6 @@ void Cycle::getTranspositionsByDiff(const vector<word>& input, word diff,
             break;
         }
     }
-}
-
-void Cycle::completeDisjoint(bool isLeftMultiplication,
-    shared_ptr<list<Transposition>> result)
-{
-    assert(result && result->size() == 1,
-        string("Cycle::completeDisjoint() called with wrong result"));
-
-    const Transposition& transp = result->front();
-
-    DisjointResult* disjointResult = (isLeftMultiplication ? &leftDisjointResult : &rightDisjointResult);
-    list<shared_ptr<Cycle>>& restCycles = disjointResult->restCycles;
-
-    assert(restCycles.size() == 1, string("Cycle::completeDisjoint() one cycle expected but failed"));
-    Cycle& cycle = *(restCycles.front());
-
-    uint complexity = uintUndefined;
-    Transposition secondTransp = cycle.findBestPair(transp, &complexity);
-
-    // push transposition to result
-    result->push_back(secondTransp);
-
-    // push transposition to cycle transpositions
-    disjointResult->transpositions->push_back(secondTransp);
-
-    // transform rest cycles to exclude
-    shared_ptr<list<Transposition>> shortTransposition(new list<Transposition>);
-    shortTransposition->push_back(secondTransp);
-
-    list<shared_ptr<Cycle>> newCycles;
-    getCyclesAfterMultiplication(isLeftMultiplication, shortTransposition,
-        restCycles, &newCycles);
-
-    disjointResult->restCycles = newCycles;
-}
-
-Transposition Cycle::findBestPair(const Transposition& transp, uint* complexity)
-{
-    word targetX = transp.getX();
-    word targetY = transp.getY();
-
-    uint elementCount = length();
-    uint stepCount = elementCount / 2;
-
-    uint minComplexity = uintUndefined;
-    Transposition secondTransp;
-
-    // debug
-    stepCount = 1;
-    for(uint step = 1; step <= stepCount; ++step)
-    {
-        uint maxIndex = elementCount;
-        if(!(elementCount & 1) && step == stepCount)
-        {
-            // avoid pair duplicates
-            maxIndex /= 2;
-        }
-
-        for(uint index = 0; index < maxIndex; ++index)
-        {
-            word x = elements[index];
-            word y = elements[modIndex(index + step)];
-
-            if(elementCount > 3 && transp.has(x) || transp.has(y))
-            {
-                // we search independent transpositions pair if possible
-                // for cycle with 3 elements only pair of dependent transpositions can be obtained
-                continue;
-            }
-
-            Transposition candidate(x, y);
-            uint complexity =
-                TransposPair(transp, candidate).getEstimateImplComplexity();
-            
-            if(complexity < minComplexity)
-            {
-                minComplexity = complexity;
-                secondTransp = candidate;
-            }
-        }
-    }
-
-    return secondTransp;
 }
 
 }   // namespace ReversibleLogic
--- Cycle.hpp
+++ Cycle.hpp
@@ -9,8 +9,6 @@ public:
     Cycle();
     Cycle(vector<word>&& source);
 
-    //Cycle& operator=(const Cycle& another) = default;
-
     // Finalize cycle: none element could be added to cycle
     void finalize();
 
@@ -42,19 +40,6 @@ public:
     /// @result - output list for adding found transpositions
     void disjointByDiff(word diff, shared_ptr<list<Transposition>> result);
 
-    // This method finds best pair for @transp in rest cycles
-    // Should be called only if there is only one transposition in result
-    void completeDisjoint(bool isLeftMultiplication,
-        shared_ptr<list<Transposition>> result);
-
-    /// @targetElements - elements from transpositions (x and y)
-    /// Returns list of new cycles if all transpositions from
-    /// disjoint result are in multiplication and sets flag
-    /// in @hasNewCycles in that case
-    list<shared_ptr<Cycle>> multiplyByTranspositions(
-        const unordered_set<word>& targetElements,
-        bool isLeftMultiplication, bool* hasNewCycles);
-
     void multiplyByTranspositions(shared_ptr<list<Transposition>> transpositions,
         bool isLeftMultiplication, vector<shared_ptr<Cycle>>* output) const;
 
@@ -63,45 +48,16 @@ private:
     uint modIndex(uint index) const;
     uint modIndex(uint index, uint mod) const;
 
-    /// Returns best pair transposition for target transposition from cycle
-    Transposition findBestPair(const Transposition& transp, uint* complexity);
-
-    vector<word> elements;
-    bool finalized;
-
     // generator 4.0 optimization
-    list<shared_ptr<Cycle>> getRestCycles(bool isLeftMultiplication);
-
-    void getCyclesAfterMultiplication(bool isLeftMultiplication,
-        shared_ptr<list<Transposition>> transpositions,
-        const list<shared_ptr<Cycle>>& cycles, list<shared_ptr<Cycle>>* result);
-
     word getOutput(word input, shared_ptr<list<Transposition>> transpositions) const;
-    word getOutput(word input, const list<shared_ptr<Cycle>>& cycles) const;
 
     /// Gets all possible transpositions with specified Hamming distance @diff
     /// from vector of elements @input and puts them to @result
     void getTranspositionsByDiff(const vector<word>& input, word diff,
         shared_ptr<list<Transposition>> result);
 
-    struct DisjointResult
-    {
-        shared_ptr<list<Transposition>> transpositions;
-        list<shared_ptr<Cycle>> restCycles;
-        uint restCyclesDistanceSum;
-    };
-
-    DisjointResult leftDisjointResult;
-    DisjointResult rightDisjointResult;
-
-    enum MultiplicationType
-    {
-        mtNone = 0,         // cycle was not multiplied by any transpositions
-        mtLeftMultiplied,   // cycle was left multiplied by transpositions
-        mtRightMultiplied,  // cycle was right multiplied by transpositions
-    };
-
-    MultiplicationType previousMultiplicationType;
+    vector<word> elements;
+    bool finalized;
 };
 
 }   // namespace ReversibleLogic
