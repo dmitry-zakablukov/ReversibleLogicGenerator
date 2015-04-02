55a54d8aa3087c9496009af69ede4fb0668a05b7
Dmitry Zakablukov <dmitriy.zakablukov@gmail.com>
2015-04-02 23:13:04 +0300

    [hwb] Usage help added

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,7 +1,31 @@
 #include "std.hpp"
 
+void printUsage(const string& fullProgramPath)
+{
+    string shortName = fullProgramPath;
+
+    auto pos = fullProgramPath.rfind('\\');
+    if (pos != string::npos)
+        shortName = shortName.substr(pos + 1);
+
+    cout << 
+        "Usage: " << shortName << " [config_file_name]\n"
+        "    config_file_name - configuration file containing options (see config.ini);\n"
+        "                       if not specified, default options would be used"
+        << endl;
+}
+
 int main(int argc, const char* argv[])
 {
+    if (argc == 2)
+    {
+        string second = argv[1];
+        if (second == "/?" || second == "-?" || second == "-h" || second == "--help")
+            printUsage(argv[0]);
+
+        return 0;
+    }
+
     Values values;
     if (argc == 2)
     {
